8b015db268c840dea4e8012f7d29a662bac1722e
joshua <jyou12@berkeley.edu>
2024-04-16 01:47:46 -0700

    update cs152configs

--- generators/chipyard/src/main/scala/config/CS152Configs.scala
+++ generators/chipyard/src/main/scala/config/CS152Configs.scala
@@ -1,49 +1,48 @@
 package chipyard
 
-import freechips.rocketchip.config.{Config}
+import org.chipsalliance.cde.config.{Config}
 import freechips.rocketchip.rocket._
 import freechips.rocketchip.subsystem._
 import freechips.rocketchip.tile.{RocketTileParams}
 
-class WithNLab5Cores(n: Int) extends Config((site, here, up) => {
-  case RocketTilesKey => {
+class WithNLab5Cores(
+  n: Int,
+  crossing: RocketCrossingParams = RocketCrossingParams()
+) extends Config((site, here, up) => {
+  case TilesLocated(InSubsystem) => {
+    val prev = up(TilesLocated(InSubsystem), site)
+    val idOffset = up(NumTiles)
     val lab5 = RocketTileParams(
-      core   = RocketCoreParams(mulDiv = Some(MulDivParams(
-        mulUnroll = 8,
-        mulEarlyOut = true,
-        divEarlyOut = true))),
       dcache = Some(DCacheParams(
         rowBits = site(SystemBusKey).beatBits,
         nSets = 16,
         nWays = 4,
-        nTLBEntries = 4,
+        nTLBSets = 1,
+        nTLBWays = 4,
         nMSHRs = 0,
         blockBytes = site(CacheBlockBytes))),
       icache = Some(ICacheParams(
         rowBits = site(SystemBusKey).beatBits,
         nSets = 64,
         nWays = 4,
-        nTLBEntries = 4,
+        nTLBSets = 1,
+        nTLBWays = 4,
         blockBytes = site(CacheBlockBytes))))
-    List.tabulate(n)(i => lab5.copy(hartId = i))
+    List.tabulate(n)(i => RocketTileAttachParams(
+      lab5.copy(tileId = i + idOffset),
+      crossing
+    )) ++ prev
   }
+  case NumTiles => up(NumTiles) + n
 })
 
 class Lab5RocketConfig extends Config(
-  new chipyard.iobinders.WithUARTAdapter ++                      // display UART with a SimUARTAdapter
-  new chipyard.iobinders.WithTieOffInterrupts ++                 // tie off top-level interrupts
-  new chipyard.iobinders.WithBlackBoxSimMem ++                   // drive the master AXI4 memory with a SimAXIMem
-  new chipyard.iobinders.WithTiedOffDebug ++                     // tie off debug (since we are using SimSerial for testing)
-  new chipyard.iobinders.WithSimSerial ++                        // drive TSI with SimSerial for testing
-  new testchipip.WithTSI ++                                      // use testchipip serial offchip link
-  new chipyard.config.WithNoGPIO ++                              // no top-level GPIO pins (overrides default set in sifive-blocks)
-  new chipyard.config.WithBootROM ++                             // use default bootrom
-  new chipyard.config.WithUART ++                                // add a UART
-  new freechips.rocketchip.subsystem.WithNoMMIOPort ++           // no top-level MMIO master port (overrides default set in rocketchip)
-  new freechips.rocketchip.subsystem.WithNoSlavePort ++          // no top-level MMIO slave port (overrides default set in rocketchip)
-  new freechips.rocketchip.subsystem.WithNExtTopInterrupts(0) ++ // no external interrupts
-  new WithNLab5Cores(1) ++                                       // single rocket core
-  new freechips.rocketchip.system.BaseConfig)                    // "base" rocketchip system
+  new freechips.rocketchip.subsystem.WithoutTLMonitors ++
+  new WithNLab5Cores(1) ++
+  new chipyard.config.WithSystemBusFrequency(500.0) ++
+  new chipyard.config.WithMemoryBusFrequency(500.0) ++
+  new chipyard.config.WithPeripheryBusFrequency(500.0) ++
+  new chipyard.config.AbstractConfig)
 
 class Lab5MESIDualRocketConfig extends Config(
   new freechips.rocketchip.subsystem.WithMESICoherence ++
